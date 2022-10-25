82adef7c0c5bf6f3eff93732b50e49f7c2253add
TobiasHameter <tobias.hameter@hotmail.com>
2022-10-25 18:30:17 +0200

    added mobile-support

--- scripts/slider.js
+++ scripts/slider.js
@@ -9,7 +9,42 @@ let isDragged = false;
 let startX;
 let scrollLeft;
 
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
 
+
+slider.addEventListener("touchtart", e => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener("touchend", (e) => {
+  isDown = false;
+  const elements = document.querySelectorAll("a");
+  if(isDragged){
+      for(let i = 0; i<elements.length; i++){
+            elements[i].addEventListener("click", preventClick);
+      }
+  }
+  else{
+      for(let i = 0; i<elements.length; i++){
+            elements[i].removeEventListener("click", preventClick);
+      }
+  }
+  slider.classList.remove("active");
+  isDragged =  false;
+});
+slider.addEventListener("touchmove", e => {
+  if (!isDown) return;
+  isDragged =  true;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX);
+  slider.scrollLeft = scrollLeft - walk;
+});
 
 
 slider.addEventListener("mousedown", e => {
@@ -17,10 +52,6 @@ slider.addEventListener("mousedown", e => {
   slider.classList.add("active");
   startX = e.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
-});
-slider.addEventListener("mouseleave", () => {
-  isDown = false;
-  slider.classList.remove("active");
 });
 slider.addEventListener("mouseup", (e) => {
   isDown = false;
